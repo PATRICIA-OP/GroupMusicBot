8529539022d455e7859c713291379a26c552a013
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-05-28 12:58:34 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -9,7 +9,7 @@ from helpers.filters import other_filters2
 async def start(_, message: Message):
     
      disable_web_page_preview=True
-    )
+    
 
 @Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
