3ff306a6f654eeeaee5641468580ba915769d3dc
PATRICIA-OP <85871907+PATRICIA-OP@users.noreply.github.com>
2021-06-14 15:18:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SESSION_NAME = getenv("SESSION_NAME", "")
+SESSION_NAME = getenv("SESSION_NAME", "AQBvV11x81WFr6s7oo_171NmUcfkRhcNw3g7uiduKarkLuvojDcyr3XAQIIpr5sJ4-Hmj4T7GWTABwGBpkq6mcnpF-1KmkajcbReETW-cX9ZdT6aoNzGg4cR7cw4T5gbSRXij-zHd7C6XOqyQO7HzXQ9VITktu6-MjLDWSdDpb9w1ibJNducIUXK9_nU8hCgC5kAmPSy1CHqJnNAPzXxgUEshMPpwjASdy-BLSXz1OIMhIYEgaOyKDtZl7EtL6kjFG90kWqS2JwVe8rpBB4gkDviZK_56IzRjJHq0RuPggPBM5cLbqJ0en54-eSK0t2a2BOO8FLCGztgZAYoaOsl0Pd1L7y8qgA")
 BOT_TOKEN = getenv("BOT_TOKEN", "1728730929:AAG_qNWSYnl1jEUvAnhaqLrXUFONUKObIJM")
 BOT_NAME = getenv("BOT_NAME", "patriciaXmusic")
 
