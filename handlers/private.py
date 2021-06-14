c7d1916f3d4a5a886c9d65f2f9b5a8c9ec79a861
PATRICIA-OP <85871907+PATRICIA-OP@users.noreply.github.com>
2021-06-14 15:00:59 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -3,25 +3,3 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from config import BOT_NAME as bn
 from helpers.filters import other_filters2
-
-
-@Client.on_message(other_filters2)
-async def start(_, message: Message):
-    
-     disable_web_page_preview=True
-    
-
-@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
-async def gstart(_, message: Message):
-      await message.reply_text("""**Group Music Player Online ✅**""",
-      reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "🔊 Channel", url="https://t.me/patricia_updates")
-                ]
-            ]
-        )
-   )
-
-
